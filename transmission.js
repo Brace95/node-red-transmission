b388e8f41565ee8f1a2cd49745d4e58073af5cfe
Brandon Stenhouse <bstenhouse27@gmail.com>
2018-12-30 02:20:12 +1100

    fixed user input error

--- transmission.js
+++ transmission.js
@@ -8,21 +8,21 @@ module.export = function (RED) {
     var node = this;
 
     // Check input and create TransmissionAPI node
-    if (this.cred && this.cred.hostname) {
+    if (this.credentials && this.credentials.hostname) {
       this.Transmission = require("transmission");
       this.TransmissionAPI = new this.Transmission({
-        host: this.cred.hostname || "127.0.0.1",
-        port: this.cred.port || 9091,
-        username: this.cred.username || "",
-        password: this.cred.password || ""
+        host: this.credentials.hostname,
+        port: this.credentials.port,
+        username: this.credentials.username,
+        password: this.credentials.password
       });
 
-      node.log( "Reauthenticating Transmission API with " + this.cred.hostname);
+      node.log( "Reauthenticating Transmission API with " + this.credentials.hostname);
     }
   }
 
   RED.nodes.registerType("transmission-config", TransmissionAPINode, {
-    cred: {
+    credentials: {
       hostname: { type:"text" },
       port: { type:"text" },
       username: { type:"text" },
